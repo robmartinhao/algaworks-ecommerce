0a3aad669e72b7ce5e7e2793cd64ba62f57d387e
Robson Martinhão <robmartinhao@gmail.com>
2022-12-21 14:00:51 -0300

    Ajuste

--- src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
+++ src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
@@ -2,15 +2,11 @@ package com.algaworks.ecommerce.model;
 
 
 import com.algaworks.ecommerce.model.enums.StatusPagamento;
-import com.algaworks.ecommerce.model.enums.StatusPedido;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
