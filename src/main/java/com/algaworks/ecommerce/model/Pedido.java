9ff2c3e16361c9aeb1bed299482e7cf1bd2d1ee7
Robson Martinhão <robmartinhao@gmail.com>
2022-12-21 16:43:12 -0300

    3.3. Exercício: mapeando a classe Pedido

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -2,8 +2,10 @@ package com.algaworks.ecommerce.model;
 
 
 import com.algaworks.ecommerce.model.enums.StatusPedido;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,14 +17,23 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
+@Table(name = "pedido")
 public class Pedido {
 
     @EqualsAndHashCode.Include
     @Id
     private Integer id;
+
+    @Column(name = "data_pedido")
     private LocalDateTime dataPedido;
+
+    @Column(name = "data_conclusao")
     private LocalDateTime dataConclusao;
+
+    @Column(name = "nota_fiscal_id")
     private Integer notaFiscalId;
+
     private BigDecimal total;
-    private StatusPedido statusPedido;
+
+    private StatusPedido status;
 }
