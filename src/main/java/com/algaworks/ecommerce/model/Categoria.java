034af0307fd0ab1fb68f50684005072dec261a98
Robson Martinhão <robmartinhao@gmail.com>
2022-12-29 08:40:45 -0300

    3.7. Conhecendo as estratégias para geração de identificador com @GeneratedValue

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -1,10 +1,7 @@
 package com.algaworks.ecommerce.model;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +15,7 @@ public class Categoria {
 
     @EqualsAndHashCode.Include
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String nome;
---
+++ src/test/java/com/algaworks/ecommerce/mapeamentobasico/EstrategiaChavePrimariaTest.java
@@ -0,0 +1,28 @@
+package com.algaworks.ecommerce.mapeamentobasico;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Categoria;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EstrategiaChavePrimariaTest extends EntityManagerTest {
+
+    @Test
+    public void testarEstrategiaAuto() {
+        Categoria categoria = new Categoria();
+        categoria.setNome("Eletrônicos");
+
+        Categoria categoria2 = new Categoria();
+        categoria2.setNome("Livros");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(categoria);
+        entityManager.persist(categoria2);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+        Assert.assertNotNull(categoriaVerificacao);
+    }
+}
