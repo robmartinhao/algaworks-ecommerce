39611980d87c45e91fda36479a97dcd0a7e54335
Robson Martinhão <robmartinhao@gmail.com>
2022-12-29 09:22:29 -0300

    3.8. Configurando a geração de identificador com @SequenceGenerator

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -15,7 +15,8 @@ public class Categoria {
 
     @EqualsAndHashCode.Include
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+    @SequenceGenerator(name = "seq", sequenceName = "sequencia_chave_primaria")
     private Integer id;
 
     private String nome;
