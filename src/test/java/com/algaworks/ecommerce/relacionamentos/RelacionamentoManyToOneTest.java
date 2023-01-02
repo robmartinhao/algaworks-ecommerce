2ff7058e8b63537329725e561dc964a13e166c62
Robson Martinhão <robmartinhao@gmail.com>
2023-01-02 20:04:17 -0300

    4.2. Mapeando relacionamentos muito-para-um com @ManyToOne

---
+++ src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
@@ -0,0 +1,35 @@
+package com.algaworks.ecommerce.relacionamentos;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.enums.StatusPedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class RelacionamentoManyToOneTest extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamento() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setCliente(cliente);
+        pedido.setTotal(BigDecimal.TEN);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+
+        Assert.assertNotNull(pedidoVerificacao);
+    }
+}
