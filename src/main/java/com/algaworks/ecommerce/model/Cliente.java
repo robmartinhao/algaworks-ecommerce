d2e550d54c3dbb64f93fc0a7e2605f9c343f20cf
Robson Martinhão <robmartinhao@gmail.com>
2023-01-23 13:35:02 -0300

    7.11. Configurando tabelas secundárias com @SecondaryTable

--- src/main/java/com/algaworks/ecommerce/model/Cliente.java
+++ src/main/java/com/algaworks/ecommerce/model/Cliente.java
@@ -16,7 +16,9 @@ import java.util.Map;
         uniqueConstraints = { @UniqueConstraint(name = "unq_cpf", columnNames = { "cpf" }) },
         indexes = { @Index(name = "idx_nome", columnList = "nome") }
 )
-@SecondaryTable(name = "cliente_detalhe", pkJoinColumns = @PrimaryKeyJoinColumn(name = "cliente_id"))
+@SecondaryTable(name = "cliente_detalhe",
+        pkJoinColumns = @PrimaryKeyJoinColumn(name = "cliente_id"),
+        foreignKey = @ForeignKey(name = "fk_cliente_detalhe_cliente"))
 public class Cliente extends EntidadeBaseInteger {
 
     @Column(length = 100, nullable = false)
