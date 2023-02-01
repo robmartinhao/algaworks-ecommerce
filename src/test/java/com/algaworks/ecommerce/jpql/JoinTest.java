4c5eff1ef42540dc9b161b501dd0c5425e28f224
Robson Martinhão <robmartinhao@gmail.com>
2023-02-01 14:07:26 -0300

    9.6. Fazendo inner join entre as entidades

---
+++ src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
@@ -0,0 +1,23 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import jakarta.persistence.TypedQuery;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class JoinTest extends EntityManagerTest {
+
+    @Test
+    public void fazerJoin() {
+        String jpql = "select p from Pedido p join p.pagamento pag";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertTrue(lista.size() == 1);
+
+    }
+}
