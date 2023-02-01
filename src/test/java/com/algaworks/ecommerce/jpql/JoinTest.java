e4de35999babd1bc8270dcfbc9516b7c87a0d02b
Robson Martinhão <robmartinhao@gmail.com>
2023-02-01 17:08:59 -0300

    9.7. Usando left outer join

--- src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
@@ -1,7 +1,6 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.Pedido;
 import jakarta.persistence.TypedQuery;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,13 +10,22 @@ import java.util.List;
 public class JoinTest extends EntityManagerTest {
 
     @Test
+    public void fazerLeftJoin() {
+        String jpql = "select p from Pedido p left join p.pagamento pag on pag.status = 'PROCESSANDO'";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
     public void fazerJoin() {
         String jpql = "select p from Pedido p join p.pagamento pag";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
         List<Object[]> lista = typedQuery.getResultList();
-        Assert.assertTrue(lista.size() == 1);
-
+        Assert.assertFalse(lista.isEmpty());
     }
 }
