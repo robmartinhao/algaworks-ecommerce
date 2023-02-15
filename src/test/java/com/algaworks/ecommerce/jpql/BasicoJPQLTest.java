621259baf1372d0bf55b797489c64855237f4f56
Robson Martinhão <robmartinhao@gmail.com>
2023-02-15 17:55:43 -0300

    9.19. Ordenando os resultados da consulta

--- src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -14,6 +14,18 @@ import java.util.List;
 public class BasicoJPQLTest extends EntityManagerTest {
 
     @Test
+    public void ordenarResultados() {
+        String jpql = "select c from Cliente c order by c.nome desc";
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(jpql, Cliente.class);
+
+        List<Cliente> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(c -> System.out.println(c.getId() + ", " + c.getNome()));
+    }
+
+    @Test
     public void projetarNoDTO() {
         String jpql = "select new com.algaworks.ecommerce.dto.ProdutoDTO(id, nome) from Produto";
         TypedQuery<ProdutoDTO> typedQuery = entityManager.createQuery(jpql, ProdutoDTO.class);
