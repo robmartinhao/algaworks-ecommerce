f3ff8897f06b832ac38130a9fd94c20c3a1f3dd6
Robson Martinhão <robmartinhao@gmail.com>
2023-02-27 15:55:44 -0300

    9.31. Usando o having para condicionar o agrupamento

--- src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -14,6 +14,24 @@ import java.util.List;
 public class GroupByTest extends EntityManagerTest {
 
     @Test
+    public void condicionarAgrupamentoComHaving() {
+//  Total de vendas dentre as categorias que mais vendem.
+
+        String jpql = "select cat.nome, sum(ip.precoProduto) from ItemPedido ip " +
+                "join ip.produto pro " +
+                "join pro.categorias cat " +
+                "group by cat.id " +
+                "having sum(ip.precoProduto) > 2000";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+    @Test
     public void agruparEFiltrarResultado() {
 
         //         Total de vendas por categoria.
