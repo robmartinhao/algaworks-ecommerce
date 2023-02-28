948bc7cebc724caa87257361d55b8f871a8fc776
Robson Martinhão <robmartinhao@gmail.com>
2023-02-28 15:32:43 -0300

    9.37. Criando subqueries com a expressão exists

--- src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
@@ -13,6 +13,21 @@ import java.util.List;
 public class SubqueriesTest extends EntityManagerTest {
 
     @Test
+    public void pesquisarComExists() {
+
+        final String jpql = "select p from Produto p " +
+                "where exists (select 1 from ItemPedido ip2 " +
+                "join ip2.produto p2 where p2 = p)";
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println("ID: " + obj.getId()));
+    }
+
+    @Test
     public void pesquisarComIN() {
 
         final String jpql = "select p from Pedido p where p.id " +
