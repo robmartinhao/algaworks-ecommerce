128dec2d0c827bbbfeb293e1658020de38751665
Robson Martinhão <robmartinhao@gmail.com>
2023-03-01 10:27:25 -0300

    9.38. Exercício: usando a expressão in

--- src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
@@ -13,6 +13,20 @@ import java.util.List;
 public class SubqueriesTest extends EntityManagerTest {
 
     @Test
+    public void pesquisarComINExercicio() {
+        String jpql = "select p from Pedido p where p.id in " +
+                " (select p2.id from ItemPedido i2 " +
+                "      join i2.pedido p2 join i2.produto pro2 join pro2.categorias c2 where c2.id = 2)";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println("ID: " + obj.getId()));
+    }
+
+    @Test
     public void pesquisarComExists() {
 
         final String jpql = "select p from Produto p " +
