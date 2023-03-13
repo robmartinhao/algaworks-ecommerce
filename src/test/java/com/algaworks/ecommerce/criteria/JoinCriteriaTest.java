4d997ab7ea3c60c2effd017a238d0b16ff29ad4e
Robson Martinhão <robmartinhao@gmail.com>
2023-03-13 14:12:00 -0300

    10.12. Passando parâmetros para as consultas

--- src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/JoinCriteriaTest.java
@@ -3,14 +3,60 @@ package com.algaworks.ecommerce.criteria;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.*;
 import com.algaworks.ecommerce.model.enums.StatusPagamento;
+import jakarta.persistence.TemporalType;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class JoinCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void passarParamentoDate() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<NotaFiscal> criteriaQuery = criteriaBuilder.createQuery(NotaFiscal.class);
+        Root<NotaFiscal> root = criteriaQuery.from(NotaFiscal.class);
+
+        ParameterExpression<Date> parameterExpressionData = criteriaBuilder.parameter(Date.class, "dataInicial");
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.greaterThan(root.get("dataEmissao"), parameterExpressionData));
+
+        TypedQuery<NotaFiscal> query = entityManager.createQuery(criteriaQuery);
+        Calendar dataInicial = Calendar.getInstance();
+        dataInicial.add(Calendar.DATE, -30);
+        query.setParameter("dataInicial", dataInicial.getTime(), TemporalType.TIMESTAMP);
+
+        List<NotaFiscal> lista = query.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(l -> System.out.println(l.getId() + " - " + l.getDataEmissao()));
+    }
+
+    @Test
+    public void passarParamento() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        ParameterExpression<Integer> parameterExpressionId = criteriaBuilder.parameter(Integer.class, "id");
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), parameterExpressionId));
+
+        TypedQuery<Pedido> query = entityManager.createQuery(criteriaQuery);
+        query.setParameter("id", 1);
+
+        Pedido pedido = query.getSingleResult();
+        Assert.assertNotNull(pedido);
+
+        System.out.println(pedido.getId() + " - " + pedido.getStatus());
+    }
+
 
     @Test
     public void buscarPedidosComProdutoEspecifico() {
