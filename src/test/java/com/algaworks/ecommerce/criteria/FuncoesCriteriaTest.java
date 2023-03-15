8b43396455febdf099ca69acab8ad4f4801ed1bc
Robson Martinhão <robmartinhao@gmail.com>
2023-03-15 12:11:40 -0300

    10.25. Usando funções para números

--- src/test/java/com/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -16,6 +16,36 @@ import java.util.List;
 public class FuncoesCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void aplicarFuncaoNumero() {
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.multiselect(
+                root.get(Pedido_.id),
+                criteriaBuilder.abs(criteriaBuilder.prod(root.get(Pedido_.id), -1)),
+                criteriaBuilder.mod(root.get(Pedido_.id), 2),
+                criteriaBuilder.sqrt(root.get(Pedido_.total))
+        );
+
+        criteriaQuery.where(criteriaBuilder.greaterThan(
+                criteriaBuilder.sqrt(root.get(Pedido_.total)), 10.0));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                arr[0] + "\n"
+                        + " abs: " + arr[1] + "\n"
+                        + " mod: " + arr[2] + "\n"
+                        + " sqrt: " + arr[3] + "\n"
+        ));
+    }
+
+    @Test
     public void aplicarFuncaoData() {
 
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
