da2652ec9ffc3892f0e756d689965374553e03e5
Robson Martinhão <robmartinhao@gmail.com>
2023-03-15 13:42:15 -0300

    10.29. Agrupando registros com o group by

---
+++ src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
@@ -0,0 +1,69 @@
+package com.algaworks.ecommerce.criteria;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.*;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
+import org.junit.Test;
+
+import java.util.List;
+
+public class GroupByCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void agruparResultado02() {
+
+        //         Total de vendas por categoria.
+        //        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.produto pro " +
+        //                 "join pro.categorias c " +
+        //                "group by c.id";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<ItemPedido> root = criteriaQuery.from(ItemPedido.class);
+        Join<ItemPedido, Produto> joinProduto = root.join(ItemPedido_.produto);
+        Join<Produto, Categoria> joinProdutoCategoria = joinProduto.join(Produto_.categorias);
+
+        criteriaQuery.multiselect(
+                joinProdutoCategoria.get(Categoria_.nome),
+                criteriaBuilder.sum(root.get(ItemPedido_.precoProduto))
+        );
+        criteriaQuery.groupBy(joinProdutoCategoria.get(Categoria_.id));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        lista.forEach(arr ->
+                System.out.println(
+                        "Nome categoria: " + arr[0]
+                                + " sum: " + arr[1]
+                ));
+    }
+
+    @Test
+    public void agruparResultado01() {
+
+        //         Quantidade de produtos por categoria.
+        //        String jpql = "select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Categoria> root = criteriaQuery.from(Categoria.class);
+        Join<Categoria, Produto> joinProduto = root.join(Categoria_.produtos, JoinType.LEFT);
+
+        criteriaQuery.multiselect(
+                root.get(Categoria_.nome),
+                criteriaBuilder.count(joinProduto.get(Produto_.id))
+        );
+        criteriaQuery.groupBy(root.get(Categoria_.id));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        lista.forEach(arr ->
+                System.out.println(
+                        "Nome: " + arr[0]
+                                + " count: " + arr[1]
+                ));
+    }
+}
