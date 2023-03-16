4a6c96dad71a2f9abb8a655258842310b73c1e3f
Robson Martinhão <robmartinhao@gmail.com>
2023-03-16 13:21:45 -0300

    10.33. Agrupando registros com funções no group by

--- src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
@@ -6,9 +6,85 @@ import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class GroupByCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void condicionarAgrupamentoComHaving() {
+
+//         Total de vendas dentre as categorias que mais vendem.
+//        String jpql = "select cat.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.produto pro join pro.categorias cat " +
+//                " group by cat.id " +
+//                " having sum(ip.precoProduto) > 100 ";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<ItemPedido> root = criteriaQuery.from(ItemPedido.class);
+        Join<ItemPedido, Produto> joinProduto = root.join(ItemPedido_.produto);
+        Join<Produto, Categoria> joinProdutoCategoria = joinProduto.join(Produto_.categorias);
+
+        criteriaQuery.multiselect(
+                joinProdutoCategoria.get(Categoria_.nome),
+                criteriaBuilder.sum(root.get(ItemPedido_.precoProduto)),
+                criteriaBuilder.avg(root.get(ItemPedido_.precoProduto))
+        );
+        criteriaQuery.groupBy(joinProdutoCategoria.get(Categoria_.id));
+
+        criteriaQuery.having(criteriaBuilder.greaterThan(
+                criteriaBuilder.avg(
+                        root.get(ItemPedido_.precoProduto)).as(BigDecimal.class), new BigDecimal(700)
+                )
+        );
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        lista.forEach(arr ->
+                System.out.println(
+                        "Nome categoria: " + arr[0]
+                                + " sum: " + arr[1]
+                                + " avg: " + arr[2]
+                ));
+    }
+
+    @Test
+    public void agruparResultadoComFuncoes() {
+//         Total de vendas por mês.
+//        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
+//                " from Pedido p " +
+//                " group by year(p.dataCriacao), month(p.dataCriacao) ";
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        Expression<Integer> anoCriacaoPedido = criteriaBuilder
+                .function("year", Integer.class, root.get(Pedido_.dataCriacao));
+        Expression<Integer> mesCriacaoPedido = criteriaBuilder
+                .function("month", Integer.class, root.get(Pedido_.dataCriacao));
+        Expression<String> nomeMesCriacaoPedido = criteriaBuilder
+                .function("monthname", String.class, root.get(Pedido_.dataCriacao));
+
+
+        Expression<String> anoMesConcat = criteriaBuilder.concat(
+                criteriaBuilder.concat(anoCriacaoPedido.as(String.class), "/"),
+                nomeMesCriacaoPedido
+        );
+
+        criteriaQuery.multiselect(
+                anoMesConcat,
+                criteriaBuilder.sum(root.get(Pedido_.total))
+        );
+
+        criteriaQuery.groupBy(anoCriacaoPedido, mesCriacaoPedido);
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        lista.forEach(arr -> System.out.println("Ano/Mês: " + arr[0] + ", Sum: " + arr[1]));
+    }
 
     @Test
     public void agruparResultado03Exercicio() {
