a5d0ad7b8df3729a5a6f2d536510995daba73189
Robson Martinhão <robmartinhao@gmail.com>
2023-03-16 13:22:55 -0300

    10.33. Agrupando registros com funções no group by

--- src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/GroupByCriteriaTest.java
@@ -12,45 +12,6 @@ import java.util.List;
 public class GroupByCriteriaTest extends EntityManagerTest {
 
     @Test
-    public void condicionarAgrupamentoComHaving() {
-
-//         Total de vendas dentre as categorias que mais vendem.
-//        String jpql = "select cat.nome, sum(ip.precoProduto) from ItemPedido ip " +
-//                " join ip.produto pro join pro.categorias cat " +
-//                " group by cat.id " +
-//                " having sum(ip.precoProduto) > 100 ";
-
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
-        Root<ItemPedido> root = criteriaQuery.from(ItemPedido.class);
-        Join<ItemPedido, Produto> joinProduto = root.join(ItemPedido_.produto);
-        Join<Produto, Categoria> joinProdutoCategoria = joinProduto.join(Produto_.categorias);
-
-        criteriaQuery.multiselect(
-                joinProdutoCategoria.get(Categoria_.nome),
-                criteriaBuilder.sum(root.get(ItemPedido_.precoProduto)),
-                criteriaBuilder.avg(root.get(ItemPedido_.precoProduto))
-        );
-        criteriaQuery.groupBy(joinProdutoCategoria.get(Categoria_.id));
-
-        criteriaQuery.having(criteriaBuilder.greaterThan(
-                criteriaBuilder.avg(
-                        root.get(ItemPedido_.precoProduto)).as(BigDecimal.class), new BigDecimal(700)
-                )
-        );
-
-        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
-        List<Object[]> lista = typedQuery.getResultList();
-
-        lista.forEach(arr ->
-                System.out.println(
-                        "Nome categoria: " + arr[0]
-                                + " sum: " + arr[1]
-                                + " avg: " + arr[2]
-                ));
-    }
-
-    @Test
     public void agruparResultadoComFuncoes() {
 //         Total de vendas por mês.
 //        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
