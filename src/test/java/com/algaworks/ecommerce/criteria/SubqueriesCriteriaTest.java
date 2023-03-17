47fd05ff37149ad243af8beee71f08645ac9c454
Robson Martinhão <robmartinhao@gmail.com>
2023-03-17 13:34:13 -0300

    10.39. Relacionando a subquery com a query principal

--- src/test/java/com/algaworks/ecommerce/criteria/SubqueriesCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/SubqueriesCriteriaTest.java
@@ -1,10 +1,7 @@
 package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.Pedido;
-import com.algaworks.ecommerce.model.Pedido_;
-import com.algaworks.ecommerce.model.Produto;
-import com.algaworks.ecommerce.model.Produto_;
+import com.algaworks.ecommerce.model.*;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -17,6 +14,37 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class SubqueriesCriteriaTest extends EntityManagerTest {
+
+
+    @Test
+    public void pesquisarSubqueries03() {
+
+//        Bons clientes.
+//        String jpql = "select c from Cliente c where " +
+//                " 500 < (select sum(p.total) from Pedido p where p.cliente = c)";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+        Root<Cliente> root = criteriaQuery.from(Cliente.class);
+
+        criteriaQuery.select(root);
+
+        Subquery<BigDecimal> subquery = criteriaQuery.subquery(BigDecimal.class);
+        Root<Pedido> subqueryRoot = subquery.from(Pedido.class);
+        subquery.select(criteriaBuilder.sum(subqueryRoot.get(Pedido_.total)));
+        subquery.where(criteriaBuilder.equal(root, subqueryRoot.get(Pedido_.cliente)));
+
+        criteriaQuery.where(criteriaBuilder.greaterThan(subquery, new BigDecimal(1300)));
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Cliente> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(
+                "ID: " + obj.getId() + " - " + ", Nome: " + obj.getNome()
+        ));
+    }
 
     @Test
     public void pesquisarSubqueries02() {
