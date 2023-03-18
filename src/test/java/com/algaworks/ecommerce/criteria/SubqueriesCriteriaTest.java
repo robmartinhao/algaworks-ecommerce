e7a2454533aa5c1bd2dfeeb781398728eac7f873
Robson Martinhão <robmartinhao@gmail.com>
2023-03-18 13:03:08 -0300

    10.44. Exercício: criando subqueries com exists

--- src/test/java/com/algaworks/ecommerce/criteria/SubqueriesCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/SubqueriesCriteriaTest.java
@@ -13,6 +13,33 @@ import java.util.List;
 public class SubqueriesCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void perquisarComExistsExercicio() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+
+        Subquery<Integer> subquery = criteriaQuery.subquery(Integer.class);
+        Root<ItemPedido> subqueryRoot = subquery.from(ItemPedido.class);
+        subquery.select(criteriaBuilder.literal(1));
+        subquery.where(
+                criteriaBuilder.equal(subqueryRoot.get(ItemPedido_.produto), root),
+                criteriaBuilder.notEqual(
+                        subqueryRoot.get(ItemPedido_.precoProduto), root.get(Produto_.preco))
+        );
+
+        criteriaQuery.where(criteriaBuilder.exists(subquery));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println("ID: " + obj.getId()));
+    }
+
+    @Test
     public void pesquisarComINExercicio() {
         // Todos o pedidos que tem algum produto da categoria de ID = 2
 
