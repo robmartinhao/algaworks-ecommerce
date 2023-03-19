9013f4b7264ad619f801ff979cb45ea9dde9d024
Robson Martinhão <robmartinhao@gmail.com>
2023-03-19 13:10:30 -0300

    10.48. Atualizando objetos em lote

---
+++ src/test/java/com/algaworks/ecommerce/criteria/OperacoesEmLoteCriteriaTest.java
@@ -0,0 +1,41 @@
+package com.algaworks.ecommerce.criteria;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Categoria;
+import com.algaworks.ecommerce.model.Categoria_;
+import com.algaworks.ecommerce.model.Produto;
+import com.algaworks.ecommerce.model.Produto_;
+import jakarta.persistence.Query;
+import jakarta.persistence.criteria.*;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class OperacoesEmLoteCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void atualizarEmLote() {
+        entityManager.getTransaction().begin();
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaUpdate<Produto> criteriaUpdate = criteriaBuilder.createCriteriaUpdate(Produto.class);
+        Root<Produto> root = criteriaUpdate.from(Produto.class);
+
+        criteriaUpdate.set(root.get(Produto_.preco),
+                criteriaBuilder.prod(root.get(Produto_.preco), new BigDecimal("1.1")));
+
+        Subquery<Integer> subquery = criteriaUpdate.subquery(Integer.class);
+        Root<Produto> subqueryRoot = subquery.correlate(root);
+        Join<Produto, Categoria> joinCategoria = subqueryRoot.join(Produto_.categorias);
+
+        subquery.select(criteriaBuilder.literal(1));
+        subquery.where(criteriaBuilder.equal(joinCategoria.get(Categoria_.id), 2));
+
+        criteriaUpdate.where(criteriaBuilder.exists(subquery));
+
+        Query query = entityManager.createQuery(criteriaUpdate);
+        query.executeUpdate();
+
+        entityManager.getTransaction().commit();
+    }
+}
