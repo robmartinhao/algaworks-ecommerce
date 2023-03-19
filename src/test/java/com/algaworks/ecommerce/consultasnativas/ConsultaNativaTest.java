36cca8d6d799f9ec1569f944b559c95c5be87b42
Robson Martinhão <robmartinhao@gmail.com>
2023-03-19 13:37:38 -0300

    11.2. Executando SQL e retornando uma lista de arrays

--- src/test/java/com/algaworks/ecommerce/consultasnativas/ConsultaNativaTest.java
+++ src/test/java/com/algaworks/ecommerce/consultasnativas/ConsultaNativaTest.java
@@ -1,8 +1,21 @@
 package com.algaworks.ecommerce.consultasnativas;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import jakarta.persistence.Query;
+import org.junit.Test;
+
+import java.util.List;
+
 public class ConsultaNativaTest extends EntityManagerTest {
 
+    @Test
+    public void executarSQL() {
+        String sql = "select id, nome from produto";
+        Query nativeQuery = entityManager.createNativeQuery(sql);
+        List<Object[]> lista = nativeQuery.getResultList();
 
-
+        lista.stream().forEach(arr -> System.out.println(
+                String.format("Produto => ID: %s, Nome: %s", arr[0], arr[1])
+        ));
+    }
 }
