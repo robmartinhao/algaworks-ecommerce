82e917c778599b0e26fdffcfc66da436cc55a575
Robson Martinhão <robmartinhao@gmail.com>
2023-03-21 18:04:21 -0300

    11.15. Invocando uma view do banco de dados

---
+++ src/test/java/com/algaworks/ecommerce/consultasnativas/ViewTest.java
@@ -0,0 +1,37 @@
+package com.algaworks.ecommerce.consultasnativas;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
+import jakarta.persistence.Query;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ViewTest extends EntityManagerTest {
+
+    @Test
+    public void executarView() {
+        Query query = entityManager.createNativeQuery(
+                "select cli.id, cli.nome, sum(ped.total) " +
+                        " from pedido ped " +
+                        " join view_clientes_acima_media cli on cli.id = ped.cliente_id " +
+                        " group by ped.cliente_id"
+        );
+
+        List<Object[]> lista = query.getResultList();
+
+        lista.stream().forEach(arr -> System.out.println(
+                String.format("Cliente => ID: %s, Nome: %s, Total: %s", arr)));
+    }
+
+    @Test
+    public void executarViewRetornandoCliente() {
+        Query query = entityManager.createNativeQuery(
+                "select * from view_clientes_acima_media", Cliente.class);
+
+        List<Cliente> lista = query.getResultList();
+
+        lista.stream().forEach(obj -> System.out.println(
+                String.format("Cliente => ID: %s, Nome: %s", obj.getId(), obj.getNome())));
+    }
+}
