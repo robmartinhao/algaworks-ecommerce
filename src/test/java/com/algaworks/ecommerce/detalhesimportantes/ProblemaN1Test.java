6042da44d2a1bd3028c96030937612b969953cb1
Robson Martinhão <robmartinhao@gmail.com>
2023-03-22 20:12:55 -0300

    12.14. Resolvendo o problema do N+1

---
+++ src/test/java/com/algaworks/ecommerce/detalhesimportantes/ProblemaN1Test.java
@@ -0,0 +1,35 @@
+package com.algaworks.ecommerce.detalhesimportantes;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import jakarta.persistence.EntityGraph;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ProblemaN1Test extends EntityManagerTest {
+
+    @Test
+    public void resolverComEntityGraph() {
+        EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+        entityGraph.addAttributeNodes("cliente", "notaFiscal", "pagamento");
+
+        List<Pedido> lista = entityManager
+                .createQuery("select p from Pedido p" , Pedido.class)
+                .setHint("jakarta.persistence.fetchgraph", entityGraph)
+                .getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void resolverComFetch() {
+        List<Pedido> lista = entityManager
+                .createQuery("select p from Pedido p " +
+                        "join fetch p.cliente c " +
+                        "join fetch p.pagamento pag " +
+                        "join fetch p.notaFiscal nf", Pedido.class)
+                .getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+}
