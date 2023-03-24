f7c9c0734e194db6301ea491239d0fe9f87da526
Robson Martinhão <robmartinhao@gmail.com>
2023-03-24 13:15:36 -0300

    14.5. Fazendo Lock Pessimista com PESSIMISTIC_READ

---
+++ src/test/java/com/algaworks/ecommerce/concorrencia/LockPessimistaTest.java
@@ -0,0 +1,115 @@
+package com.algaworks.ecommerce.concorrencia;
+
+import com.algaworks.ecommerce.model.Produto;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.Persistence;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LockPessimistaTest {
+
+    protected static EntityManagerFactory entityManagerFactory;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        entityManagerFactory = Persistence
+                .createEntityManagerFactory("Ecommerce-PU");
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() {
+        entityManagerFactory.close();
+    }
+
+    public static void log(Object obj, Object... args) {
+        System.out.println(
+                String.format("[LOG " + System.currentTimeMillis() + "] " + obj, args)
+        );
+    }
+
+    public static void esperar(int segundos) {
+        try {
+            Thread.sleep(segundos * 1000);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void usarLockPessimistaLockModeTypePessimisticRead() {
+        Runnable runnable1 = () -> {
+            log("Iniciando Runnable 01.");
+
+            String novaDescricao = "Descrição detalhada. CTM: " + System.currentTimeMillis();
+
+            EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+            entityManager1.getTransaction().begin();
+
+            log("Runnable 01 vai carregar o produto 1.");
+            Produto produto = entityManager1.find(Produto.class, 1, LockModeType.PESSIMISTIC_READ);
+
+            log("Runnable 01 vai alterar o produto.");
+            produto.setDescricao(novaDescricao);
+
+            log("Runnable 01 vai esperar por 3 segundos.");
+            esperar(3);
+
+            log("Runnable 01 vai confirmar a transação.");
+            entityManager1.getTransaction().commit();
+            entityManager1.close();
+
+            log("Encerrando Runnable 01.");
+        };
+
+        Runnable runnable2 = () -> {
+            log("Iniciando Runnable 02.");
+
+            String novaDescricao = "Descrição massa! CTM: " + System.currentTimeMillis();
+
+            EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+            entityManager2.getTransaction().begin();
+
+            log("Runnable 02 vai carregar o produto 1.");
+            Produto produto = entityManager2.find(Produto.class, 1);
+
+            log("Runnable 02 vai alterar o produto.");
+            produto.setDescricao(novaDescricao);
+
+            log("Runnable 02 vai esperar por 1 segundo.");
+            esperar(1);
+
+            log("Runnable 02 vai confirmar a transação.");
+            entityManager2.getTransaction().commit();
+            entityManager2.close();
+
+            log("Encerrando Runnable 02.");
+        };
+
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+
+        thread1.start();
+
+        esperar(1);
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        EntityManager entityManager3 = entityManagerFactory.createEntityManager();
+        Produto produto = entityManager3.find(Produto.class, 1);
+        entityManager3.close();
+
+        Assert.assertTrue(produto.getDescricao().startsWith("Descrição detalhada."));
+
+        log("Encerrando método de teste.");
+    }
+}
