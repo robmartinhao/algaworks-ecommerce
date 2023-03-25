fa30fbe675242490c1b3d1951f20d7e806948341
Robson Martinhão <robmartinhao@gmail.com>
2023-03-25 13:40:16 -0300

    15.3. Criando um novo schema no banco de dados

--- src/main/java/com/algaworks/ecommerce/util/ExecutarDDL.java
+++ src/main/java/com/algaworks/ecommerce/util/ExecutarDDL.java
@@ -10,6 +10,10 @@ public class ExecutarDDL {
     public static void main(String[] args) {
         Map<String, String> propriedades = new HashMap<>();
 
+        //Comentar esse linha de configuração no arquivo persistence.xml para funcionar
+        propriedades.put("jakarta.persistence.jdbc.url",
+                "jdbc:mysql://localhost/loja_ecommerce?createDatabaseIfNotExist=true&useTimezone=true&serverTimezone=UTC");
+
         propriedades.put("javax.persistence.schema-generation.database.action",
                 "drop-and-create");
 
